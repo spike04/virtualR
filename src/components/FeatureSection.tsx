bc47301748bb282ee4fcfefba2f1dffe2b8f7c05
spike04 <rubinbajracharya@yahoo.com>
2024-06-28 12:11:09 +0545

    Initial Website Design Template

---
+++ src/components/FeatureSection.tsx
@@ -0,0 +1,36 @@
+import { features } from '../constants'
+
+export default function FeatureSection() {
+  return (
+    <div className="relative mt-20 border-neutral-800 min-h-[800px]">
+      <div className="text-center pb-8">
+        <h1 className="text-base font-semibold leading-7 text-orange-500">
+          Features
+        </h1>
+        <p className="mt-2 text-4xl font-bold tracking-tight text-white sm:text-5xl">
+          Easy Build{' '}
+          <span className="bg-gradient-to-r from-orange-500 to-orange-800 text-transparent bg-clip-text">
+            your code
+          </span>
+        </p>
+      </div>
+      <div className="flex flex-wrap mt-10 lg:mt-20">
+        {features.map((feature, index) => (
+          <div key={index} className="w-full sm:w-1/2 lg:w-1/3">
+            <div className="flex">
+              <div className="flex mx-6 h-10 w-10 p-2 bg-neutral-900 text-orange-700 justify-center items-center rounded-full">
+                {feature.icon}
+              </div>
+              <div>
+                <h5 className="mt-1 mb-6 text-xl">{feature.text}</h5>
+                <p className="text-md p-2 mb-20 text-neutral-500">
+                  {feature.description}
+                </p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,69 @@
+export default function HeroSection() {
+  return (
+    <div className="text-white h-screen -mb-40">
+      <div
+        className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+        aria-hidden="true"
+      >
+        <div
+          className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+          style={{
+            clipPath:
+              'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+          }}
+        />
+      </div>
+      <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-32">
+        <div className="hidden sm:mb-8 sm:flex sm:justify-center">
+          <div className="rounded-full px-3 py-1 text-sm leading-6 text-gray-200 ring-1 ring-orange-100/10 hover:ring-orange-100/20">
+            Announcing our next round of funding.{' '}
+            <a href="#" className="font-semibold text-orange-500">
+              <span className="absolute inset-0" aria-hidden="true" />
+              Read more <span aria-hidden="true">&rarr;</span>
+            </a>
+          </div>
+        </div>
+        <div className="text-center">
+          <h1 className="text-4xl font-bold tracking-tight text-orange-500 sm:text-6xl">
+            VirtualR build tools
+            <span className="bg-gradient-to-r from-orange-500 to-red-800 text-transparent bg-clip-text">
+              {' '}
+              for developers
+            </span>
+          </h1>
+          <p className="mt-6 text-lg leading-8 text-gray-200">
+            Empower your creativity and bring your VR app ideas to life with our
+            intuitive development tools. Get started today and turn your
+            imagination into immersive reality!
+          </p>
+          <div className="mt-10 flex items-center justify-center gap-x-6">
+            <a
+              href="#"
+              className="rounded-md bg-orange-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600"
+            >
+              Start for Free
+            </a>
+            <a
+              href="#"
+              className="text-sm font-semibold leading-6 text-orange-500"
+            >
+              Documentation <span aria-hidden="true">→</span>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div
+        className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+        aria-hidden="true"
+      >
+        <div
+          className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
+          style={{
+            clipPath:
+              'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
+          }}
+        />
+      </div>
+    </div>
+  )
+}
